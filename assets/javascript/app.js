d9922256b1f171c377f2053f3b5684ae71b452a3
gitsunitha <sunitha.pillai@gmail.com>
2020-05-16 20:47:53 -0400

    Checking in Chris_branch changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,54 @@
-$('#myModal').on('shown.bs.modal', function() {
-    $('#myInput').trigger('focus')
-});
+// $('#myModal').on('shown.bs.modal', function() {
+//     $('#myInput').trigger('focus')
+// });
+
+//------------------------------------------------------------------------------
+
+$(document).ready(function(){
+
+    $("#search").hide(); 
+
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //Adding click actions for "Continue"
+
+    var button = document.getElementById("options1");
+   
+    button.onclick = function (){
+
+        $("#search").show();
+        $(".modal-content").hide();
+    }
+
+
+});  // Ends document ready
+
+
+//------------------------------------------------------------------------------------------------
 
 var globalJobs = 0;
 
